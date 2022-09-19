ad469c5e7c483d70dc56df14717b8f9c861b761e
Anik Das <cse2019023@rcciit.org.in>
2022-09-19 11:49:20 +0530

    Update Prescriptions.jsx

--- probook-client/src/components/Prescriptions.jsx
+++ probook-client/src/components/Prescriptions.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { getProfile } from "../redux/authSlice";
 
 const Prescriptions = () => {
+  const { _id } = JSON.parse(localStorage.getItem("login"));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProfile(_id));
+  }, [dispatch, _id]);
+  const { profile } = useSelector((state) => state.auth);
+  const [loginAs,setLoginAs] = useState(profile?.userId?.loginAs)
+
   return (
-    <div>
-      hello
-    </div>
+    <>
+    {loginAs === 'doctor' ? (<h1>Upload Certificates</h1>):(<h1>Upload Prescriptions</h1>)}
+    </>
   )
 }
 
